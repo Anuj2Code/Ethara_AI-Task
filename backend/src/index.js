058d28bf6f00858b15fbad4a47f9377c6866b6e7
Anuj2Code <tanujpal2004@gmail.com>
2026-05-04 19:07:57 +0530

    fix: move vite to devDependencies

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,10 +26,11 @@ app.use('/api/projects', require('./routes/projects'));
 app.use('/api/tasks', require('./routes/tasks'));
 
 // Serve frontend
+// DELETE this entire block
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+  app.use(express.static(path.join(__dirname, '../../../frontend/dist')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../../frontend/dist/index.html'));
+    res.sendFile(path.resolve(__dirname, '../../../frontend/dist/index.html'));
   });
 }
 
